7c35e022ee233aa2804fad4948b5b837942c903a
Misha Evdokimov <evdokimovmm1@yandex.ru>
2025-02-25 22:02:20 +0300

    Добавлены юнит тесты на класс Lion

---
+++ src/test/java/LionTest.java
@@ -0,0 +1,46 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Mock
+    Feline feline;
+
+//    Lion lion = new Lion(feline);
+
+    @Test
+    public void getKittensSuccess() {
+        Lion lion = new Lion(feline);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        Assert.assertEquals(1, lion.getKittens());
+    }
+
+    @Test
+    public void doesHaveManeTrue() throws Exception {
+        Lion lion = new Lion("Самец");
+        Assert.assertTrue(lion.doesHaveMane());
+    }
+
+    @Test
+    public void doesHaveManeFalse() throws Exception {
+        Lion lion = new Lion("Самка");
+        Assert.assertFalse(lion.doesHaveMane());
+    }
+
+    @Test
+    public void getFoodSuccess() throws Exception {
+        Lion lion = new Lion(feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
+    }
+
+}
