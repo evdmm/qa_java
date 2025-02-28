c472ecf910e0ee6bcf972ffb3a615df292c85b81
Misha Evdokimov <evdokimovmm1@yandex.ru>
2025-02-28 11:32:21 +0300

    Фикс. Исправлены юнит тесты на класс Lion с учетом исправленного конструктора

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -19,32 +19,32 @@ public class LionTest {
 
     @Test
     public void createLionException() {
-        Exception exception = assertThrows(Exception.class, () -> new Lion("Неизвестный"));
+        Exception exception = assertThrows(Exception.class, () -> new Lion(new Feline(),"Неизвестный"));
         Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
     }
 
     @Test
-    public void getKittensSuccess() {
-        Lion lion = new Lion(feline);
+    public void getKittensSuccess() throws Exception {
+        Lion lion = new Lion(feline, "Самец");
         Mockito.when(feline.getKittens()).thenReturn(1);
         Assert.assertEquals(1, lion.getKittens());
     }
 
     @Test
     public void doesHaveManeTrue() throws Exception {
-        Lion lion = new Lion("Самец");
+        Lion lion = new Lion(feline,"Самец");
         Assert.assertTrue(lion.doesHaveMane());
     }
 
     @Test
     public void doesHaveManeFalse() throws Exception {
-        Lion lion = new Lion("Самка");
+        Lion lion = new Lion(feline,"Самка");
         Assert.assertFalse(lion.doesHaveMane());
     }
 
     @Test
     public void getFoodSuccess() throws Exception {
-        Lion lion = new Lion(feline);
+        Lion lion = new Lion(feline, "Самка");
         Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
     }
