bc7338f0b957314b25a4eac9295100450da9197b
Misha Evdokimov <evdokimovmm1@yandex.ru>
2025-02-28 11:23:40 +0300

    Фикс. Исправлен конструктор класса Lion

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -7,7 +7,8 @@ public class Lion {
     boolean hasMane;
     private Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(Feline feline, String sex) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -15,10 +16,6 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
-    }
-
-    public Lion(Feline feline) {
-        this.feline = feline;
     }
 
     public int getKittens() {
